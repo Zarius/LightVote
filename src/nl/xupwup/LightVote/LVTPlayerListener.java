137bdca9dc6a3f2825d827a3efbefd5afa6e5d1d
Zarius <zariust@gmail.com>
2011-05-09 22:43:08 +1000

    first commit of code as passed over from xupwup (thanks for the great plugin)

---
+++ src/nl/xupwup/LightVote/LVTPlayerListener.java
@@ -0,0 +1,278 @@
+package nl.xupwup.LightVote;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Handle events for all Player related events
+ * @author XUPWUP
+ */
+public class LVTPlayerListener {
+    private final LightVote plugin;
+    private Logger log;
+
+	private double reqYesVotes, minAgree;
+	private int permaOffset; 
+	private int voteTime, voteFailDelay, votePassDelay, voteRemindCount;
+	private boolean perma;
+	private static final int nightstart = 14000;
+	private Set<String> canStartVotes = null;
+    
+    public LVTPlayerListener(LightVote instance, Logger log) {
+        plugin = instance;
+        this.log = log;
+    }
+	
+    
+    public void config(double reqYesVotes, double minAgree, int permaOffset, int voteTime, int voteFailDelay, int votePassDelay, int voteRemindCount, boolean perma, Set<String> set){
+    	this.reqYesVotes = reqYesVotes;
+    	this.minAgree = minAgree;
+    	this.permaOffset = permaOffset;
+    	this.voteTime = voteTime;
+    	this.voteFailDelay = voteFailDelay;
+    	this.votePassDelay = votePassDelay;
+    	this.voteRemindCount = voteRemindCount;
+    	this.perma = perma;
+    	canStartVotes = set;
+    }
+    
+    private int agrees = 0;
+	private boolean day = true;
+	private int remindCounter = 0;
+	private boolean disabled = false;
+	private boolean voting = false;
+	Timer tReset = null;
+	
+	private HashSet<Player> voters = new HashSet<Player>();
+	
+	Timer t = new Timer();
+	Timer reminder;
+	
+	
+	private boolean isDay(long currenttime){
+		return ((currenttime % 24000) < 12000 && currenttime > 0 )|| (currenttime < 0 && (currenttime % 24000) < -12000);
+	}
+	
+	
+	public void setReset(){
+		tReset = new Timer();
+    	tReset.schedule(new timeReset(), 15000, 15000);
+	}
+	
+	private class timeReset extends TimerTask{
+		public void run(){
+			long currenttime = plugin.getServer().getWorlds().get(0).getTime();
+			boolean isNight = !isDay(currenttime);
+			currenttime = currenttime - (currenttime % 24000); // one day lasts 24000
+			currenttime += permaOffset;
+			if (isNight) currenttime += nightstart;
+			plugin.getServer().getWorlds().get(0).setTime(currenttime);
+		}
+	}
+	
+	
+	private class voteEnd extends TimerTask{
+		public void run(){
+			endVote();
+		}
+	}
+	
+	private class reEnable extends TimerTask{
+		public void run(){
+			disabled = false;
+		}
+	}
+	
+	private class remind extends TimerTask{
+		public void run(){
+			int timeBetween = voteTime / (voteRemindCount+1);
+			remindCounter++;
+			if (remindCounter > voteRemindCount) {
+				reminder.cancel();
+				return;
+			}
+			
+			plugin.getServer().broadcastMessage(ChatColor.GOLD + "Vote for " + (day ? "day" : "night") + ", " + (voteTime - remindCounter*timeBetween)/1000 + " seconds remaining.");
+		}
+	}	
+	
+	private void endVote(){
+		
+		Player[] playerlist = plugin.getServer().getOnlinePlayers();
+		String msg = "";
+		boolean passed = false;
+		
+		int numplayers = playerlist.length;
+		
+		if (voters.size() > numplayers * reqYesVotes){
+			if (agrees > minAgree * voters.size()) {
+				msg = "Vote passed. (" + agrees + " yes, " + (voters.size() - agrees) + " no)";
+				long currenttime = plugin.getServer().getWorlds().get(0).getTime();
+				currenttime = currenttime - (currenttime % 24000); // one day lasts 24000
+				
+				if (currenttime < 0){
+					currenttime *= -1;
+					log.info("LVT: Current time was negative!");
+				}
+				
+				if (!day) currenttime += nightstart;
+				if(perma) currenttime += permaOffset;
+				
+				plugin.getServer().getWorlds().get(0).setTime(currenttime);
+				passed = true;
+				log.info("LVT: changed time to "+ (day ? "day" : "night"));
+			}
+			else {
+				msg = "Vote failed. (" + agrees + " yes, " + (voters.size() - agrees) + " no)";
+				log.info("LVT: vote failed (" + voters.size() + " votes, "+ agrees + " agree)");
+			}
+		}else{
+			msg = "Vote failed, insufficient \"yes\" votes. (" + agrees + "/" + (numplayers * reqYesVotes) + ")";
+			log.info("LVT: vote failed, insufficient votes (" + agrees + " yes votes, "+ numplayers + " players, req " + (numplayers * reqYesVotes)+ ")");
+		}
+		
+		plugin.getServer().broadcastMessage(ChatColor.GOLD + msg);
+		agrees = 0;
+		voters = new HashSet<Player>();
+		voting = false;
+		disabled = true;
+		Timer reenable = new Timer();
+		reenable.schedule(new reEnable(), (passed ? votePassDelay : voteFailDelay));
+	}
+	
+	public boolean canSVote(CommandSender sender){
+		if(sender instanceof Player) {
+			return canStartVotes == null || canStartVotes.contains(((Player) sender).getName().toLowerCase());
+		}else return true;
+	}
+	
+	public boolean onPlayerCommand(CommandSender sender, Command command,
+    		String label, String[] args){
+		
+		String[] split = args;
+		if (!label.equalsIgnoreCase("lvt")) return false;
+
+		if (split.length == 0 || (split.length == 1 && split[0].equalsIgnoreCase("help"))){
+			sender.sendMessage(ChatColor.GOLD + "Lightvote commands");
+			if(canSVote(sender)) {
+				sender.sendMessage(ChatColor.GOLD + "/lvt start -- start a vote(for day)");
+				sender.sendMessage(ChatColor.GOLD + "/lvt start night -- start a vote for night");
+			}
+			sender.sendMessage(ChatColor.GOLD + "/lvt yes/no -- vote");
+			sender.sendMessage(ChatColor.GOLD + "/lvt help -- this message");
+			sender.sendMessage(ChatColor.GOLD + "/lvt info -- some information");
+			return true;
+		}
+		
+		if(split[0].equalsIgnoreCase("info")){
+			sender.sendMessage(ChatColor.GOLD + "Lightvote created by XUPWUP");
+			sender.sendMessage(ChatColor.GOLD + "Lightvote version " + plugin.getDescription().getVersion());
+			sender.sendMessage(ChatColor.GOLD + "Static time is " + (perma ? "enabled" : "disabled"));
+			sender.sendMessage(ChatColor.GOLD + "Current time:" + plugin.getServer().getWorlds().get(0).getTime()%24000 );
+			return true;
+		}
+		
+		if (split[0].equalsIgnoreCase("start")){
+			if(!canSVote(sender)){
+				sender.sendMessage(ChatColor.GOLD + "You are not allowed to start votes.");
+				return true;
+			}
+			if (voting) {
+				sender.sendMessage(ChatColor.GOLD + "A vote is still in progress.");
+				return true;
+			}
+			
+			if (disabled){
+				sender.sendMessage(ChatColor.GOLD + "You cannot vote again this quickly.");
+				return true;
+			}
+			
+			if (split.length > 1){
+				if (split[1].equalsIgnoreCase("night")) day = false;
+				else day = true;
+			}else day = true;
+			
+			long currenttime = plugin.getServer().getWorlds().get(0).getTime();				
+			
+			if (isDay(currenttime)){ // it is day now
+				if (day){
+					sender.sendMessage(ChatColor.GOLD + "It is already day!");
+					return true;
+				}
+			}else{ // it is night now
+				if (!day){
+					sender.sendMessage(ChatColor.GOLD + "It is already night!");
+					return true;
+				}
+			}
+			
+			String daymsg = "";
+			if (day){
+				daymsg = "for daylight";
+			}else daymsg = "for darkness";
+			
+			String pname;
+			if(sender instanceof Player) {
+				pname =((Player) sender).getName();
+				voters.add((Player) sender);
+				agrees++;
+			}else pname = "<CONSOLE>";
+			voting = true;
+			plugin.getServer().broadcastMessage(ChatColor.GOLD + "Lightvote " + daymsg + " started by "+ pname + ",");
+			plugin.getServer().broadcastMessage(ChatColor.GOLD + "type /lvt yes, or /lvt no to vote.");
+			
+			t.schedule(new voteEnd(), voteTime);
+			if (voters.size() == plugin.getServer().getOnlinePlayers().length){
+				t.cancel();
+				t = new Timer();
+				endVote();
+				return true;
+			}
+			
+			reminder = new Timer();
+			
+			if (voteRemindCount > 0){
+				remindCounter = 0;
+				int timeBetween = voteTime / (voteRemindCount+1);
+				reminder.schedule(new remind(), timeBetween, timeBetween);
+			}
+			return true;
+		}else{
+			if(sender instanceof Player) if (voters.contains((Player) sender)){
+				sender.sendMessage(ChatColor.GOLD + "You have already voted");
+				return true;
+			}
+			if (!voting){
+				sender.sendMessage(ChatColor.GOLD + "No votes in progress, use /lvt start to start a vote");
+				return true;
+			}
+			
+			boolean agreed = false;
+			if (split[0].equalsIgnoreCase("yes") || split[0].equalsIgnoreCase("y")) {
+				agrees++;
+				agreed = true;
+			}else if (! (split[0].equalsIgnoreCase("no") || split[0].equalsIgnoreCase("n"))) {
+				sender.sendMessage(ChatColor.GOLD + "Invalid answer \"" + split[0] + "\", please use \"y\", or \"n\"");
+				return true;
+			}
+			
+			if(sender instanceof Player) voters.add((Player)sender);
+			if (voters.size() == plugin.getServer().getOnlinePlayers().length){
+				t.cancel();
+				t = new Timer();
+				reminder.cancel();
+				endVote();
+			}
+			sender.sendMessage(ChatColor.GOLD + "Thanks for voting! (" + (agreed ? "yes" : "no") + ")");
+		}
+		return true;
+	}
+}
---
+++ src/nl/xupwup/LightVote/LightVote.java
@@ -0,0 +1,178 @@
+package nl.xupwup.LightVote;
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * LightVote for Bukkit
+ *
+ * @author XUPWUP
+ */
+public class LightVote extends JavaPlugin {
+    private LVTPlayerListener playerListener;
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+	private Logger log;
+    private String name;
+    private String version;
+    File voteStartersFile;
+    HashSet<String> voters;
+    
+    // default configuration
+    private double reqYesVotes = 0.05, minAgree = 0.5;
+	private int permaOffset = 4000; 
+	private int voteTime = 30000, voteFailDelay = 30000, votePassDelay = 50000, voteRemindCount = 2;
+	private boolean perma = false;
+	private static final String defaultConfig = "# At least 'required-yes-percentage'*peopleOnServer people must vote yes, and there must be more people that voted yes than no" + '\n' + 
+		"required-yes-percentage 5" + '\n' +
+	 	"minimum-agree-percentage 50" + '\n' +
+		"vote-fail-delay 30" + '\n' +
+		"vote-pass-delay 50" + '\n' +
+		"vote-time 30" + '\n' +
+		"reminders 2" + '\n' +
+		"permanent no";
+    
+    private void parseSettings(Scanner sc){ 
+		while(sc.hasNext()){
+			String thisline = sc.nextLine();
+			String[] contents = thisline.split(" ");
+			if (contents.length > 1){
+				if (contents[0].equals("minimum-agree-percentage")){
+						minAgree = Integer.parseInt(contents[1]);
+						minAgree /= 100;
+				}else if (contents[0].equals("required-yes-percentage")){
+						reqYesVotes = Integer.parseInt(contents[1]);
+						reqYesVotes /= 100;
+				}else if (contents[0].equals("vote-fail-delay")){
+						voteFailDelay = Integer.parseInt(contents[1]) * 1000;
+				}else if (contents[0].equals("vote-pass-delay")){
+						votePassDelay = Integer.parseInt(contents[1]) * 1000;
+				}else if (contents[0].equals("vote-time")){
+						voteTime = Integer.parseInt(contents[1]) * 1000;
+				}else if (contents[0].equals("reminders")){
+						voteRemindCount = Integer.parseInt(contents[1]);
+				}else if (contents[0].equals("permanent")){
+						perma = contents[1].equals("yes");
+				}
+			}
+		}
+	}
+
+   
+
+    public void onEnable() {
+    	PluginDescriptionFile pdfFile = this.getDescription();
+        name = pdfFile.getName();
+        version = pdfFile.getVersion();
+        
+    	log = Logger.getLogger("Minecraft");        
+        playerListener = new LVTPlayerListener(this, log);
+        log.info(name + " " + version + " initialized");
+    	
+        
+        File folder = new File("plugins" + File.separator + "LightVote");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        
+        File configFile = new File(folder.getAbsolutePath() + File.separator +"LightVote.conf");
+        if (configFile.exists()){
+        	log.info(name + " " + version + " scanning properties file.");
+        	Scanner sc = null;
+        	try {
+				sc = new Scanner(configFile);
+				parseSettings(sc);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+        }else{
+        	log.info(name + " " + version + " creating properties file.");
+        	BufferedWriter out = null;
+        	try {
+				configFile.createNewFile();
+				out = new BufferedWriter(new FileWriter(configFile));
+				out.write(defaultConfig);
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        }
+        
+        voteStartersFile = new File(folder.getAbsolutePath() + File.separator +"voteStarters.conf");
+        if(voteStartersFile.exists()){
+        	updateVoters(voteStartersFile);
+        }else{
+        	BufferedWriter out = null;
+        	try {
+				voteStartersFile.createNewFile();
+				out = new BufferedWriter(new FileWriter(voteStartersFile));
+				out.write("*");
+				out.close();
+				voters = null;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        }
+        
+        playerListener.config(reqYesVotes, minAgree, permaOffset, voteTime, voteFailDelay, votePassDelay, voteRemindCount, perma, voters);
+
+        if(perma){
+        	playerListener.setReset();
+        }
+    }
+    
+    private void updateVoters(File f){
+    	try {
+			Scanner sc = new Scanner(f);
+			voters = new HashSet<String>();
+			while(sc.hasNext()){
+				String name = sc.next();
+				if (name.equals("*")){
+					voters = null;
+					return;
+				}
+				voters.add(name.toLowerCase());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command,
+    		String label, String[] args) {
+
+    	return playerListener.onPlayerCommand(sender, command, label, args);
+    }
+    public void onDisable() {
+    	if (playerListener.tReset != null) playerListener.tReset.cancel();
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        System.out.println( name + " version " + version + " disabled." );
+    }
+    public boolean isDebugging(final Player player) {
+        if (debugees.containsKey(player)) {
+            return debugees.get(player);
+        } else {
+            return false;
+        }
+    }
+
+    public void setDebugging(final Player player, final boolean value) {
+        debugees.put(player, value);
+    }
+}
